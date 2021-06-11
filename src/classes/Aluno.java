49aa95d1a85dbd524d9d16f63521f9dc38e0728e
Fernando <fernandofalcone_02@hotmail.com>
2021-06-11 14:58:45 -0300

    Add constants

--- src/classes/Aluno.java
+++ src/classes/Aluno.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import constantes.StatusAluno;
+
 public class Aluno {
     private String nome;
     private int idade;
@@ -148,12 +150,12 @@ public class Aluno {
         double media = this.calcularMedia();
         if (media >= 50) {
             if (media >= 70) {
-                return "Aluno está aprovado";
+                return StatusAluno.APROVADO;
             } else {
-                return "Aluno está em recuperação";
+                return StatusAluno.RECUPERACAO;
             }
         }
-        return "Aluno está reprovado";
+        return StatusAluno.REPROVADO;
     }
 
 }
