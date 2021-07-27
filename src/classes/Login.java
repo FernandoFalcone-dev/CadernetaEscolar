0bb475eff6511fc7e87a66ae9620535b938b64ce
Fernando <fernandofalcone_02@hotmail.com>
2021-07-27 14:14:13 -0300

    Implementando HashMap e Login

---
+++ src/classes/Login.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class Login {
+    private String usuario;
+    private String senha;
+
+    public String getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+}
